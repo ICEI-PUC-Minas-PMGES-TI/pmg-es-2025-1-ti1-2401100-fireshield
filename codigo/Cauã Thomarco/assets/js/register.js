6be647dd7f1ebee927e4f725b3348ff60a9e421e
Cauã Thomarco <caua.teixeira@sga.pucminas.br>
2025-06-01 23:11:40 -0300

    Mudança de LocalStorage para JsonServer

---
+++ codigo/Cauã Thomarco/assets/js/register.js
@@ -0,0 +1,27 @@
+document.getElementById('register-form').addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const novoUsuario = {
+    nome: document.getElementById('nome').value,
+    email: document.getElementById('Email').value,
+    cpf: document.getElementById('cpf').value,
+    cidade: document.getElementById('cidade').value,
+    estado: document.getElementById('estado').value,
+    pais: document.getElementById('pais').value,
+    rua: document.getElementById('rua').value,
+    senha: document.getElementById('senha').value
+  };
+
+  const response = await fetch("http://localhost:3000/usuarios", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(novoUsuario)
+  });
+
+  if (response.ok) {
+    alert("Usuário registrado com sucesso!");
+    window.location.href = "login.html";
+  } else {
+    alert("Erro ao registrar usuário.");
+  }
+});
